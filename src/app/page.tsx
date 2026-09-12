4b4e6185179bc3b6d89939ae7c85a7801be351d4
Yelda Yaren <yeldayaren@unknown3e8ab0e90b51.home>
2026-09-12 17:09:50 +0300

    fix(landing): resolve mobile and tablet logo overflow in navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -56,7 +56,7 @@ export default function LandingPage() {
               alt="Dermofis"
               width={300}
               height={80}
-              className="h-16 md:h-20 w-auto scale-[1.8] lg:scale-[2.2] origin-left"
+              className="h-10 lg:h-20 w-auto scale-[1.4] lg:scale-[2.2] origin-left"
               priority
             />
           </Link>
@@ -72,7 +72,7 @@ export default function LandingPage() {
                   alt="Dermofis"
                   width={240}
                   height={64}
-                  className="h-16 w-auto scale-[1.8] origin-left"
+                  className="h-10 w-auto scale-[1.4] origin-left"
                   priority
                 />
               </Link>
