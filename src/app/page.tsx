6425918821c46af4c92fbb5f69d145a2433cc0d8
Yelda Yaren <yeldayaren@unknown3e8ab0e90b51.home>
2026-09-12 14:54:39 +0300

    Crop logo tightly and match height with CTA button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -56,7 +56,7 @@ export default function LandingPage() {
               alt="Dermofis"
               width={300}
               height={80}
-              className="h-16 md:h-20 w-auto scale-125 origin-left"
+              className="h-12 lg:h-[72px] w-auto"
               priority
             />
           </Link>
@@ -72,7 +72,7 @@ export default function LandingPage() {
                   alt="Dermofis"
                   width={240}
                   height={64}
-                  className="h-16 w-auto scale-125 origin-left"
+                  className="h-12 lg:h-[60px] w-auto"
                   priority
                 />
               </Link>
