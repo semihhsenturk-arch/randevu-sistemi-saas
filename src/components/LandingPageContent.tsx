a318da6fd6478701f9e837c64c71c6283b099727
Yelda Yaren <yeldayaren@unknown3e8ab0e90b51.home>
2026-09-12 19:07:25 +0300

    fix(landing): scale up logo sizes across all breakpoints while maintaining precise translate-y alignment

--- src/components/LandingPageContent.tsx
+++ src/components/LandingPageContent.tsx
@@ -56,7 +56,7 @@ export default function LandingPageContent() {
               alt="Dermofis"
               width={300}
               height={80}
-              className="w-[160px] h-auto object-contain translate-y-3"
+              className="w-[200px] h-auto object-contain translate-y-4"
               priority
             />
           </Link>
@@ -68,7 +68,7 @@ export default function LandingPageContent() {
               alt="Dermofis"
               width={300}
               height={80}
-              className="w-[250px] h-auto object-contain translate-y-6"
+              className="w-[320px] h-auto object-contain translate-y-8"
               priority
             />
           </Link>
@@ -84,7 +84,7 @@ export default function LandingPageContent() {
                   alt="Dermofis"
                   width={240}
                   height={64}
-                  className="w-[200px] h-auto object-contain translate-y-5"
+                  className="w-[250px] h-auto object-contain translate-y-6"
                   priority
                 />
               </Link>
